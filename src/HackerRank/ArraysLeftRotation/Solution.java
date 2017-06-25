c19211393b52b9027efb24e89f99f74bcd001959
Akash Ravindranath <akashravindranath@gmail.com>
2017-06-24 23:22:34 -0700

    Leet Code and Hacker Rank

--- src/HackerRank/ArraysLeftRotation/Solution.java
+++ src/HackerRank/ArraysLeftRotation/Solution.java
@@ -1,8 +1,25 @@
 package HackerRank.ArraysLeftRotation;
 
+import java.util.Scanner;
+
 /**
  * Created by Akashravindranath on 6/24/17.
  */
 public class Solution {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+
+        Solution s = new Solution();
+    }
+
+    private void Helper(int n, int k, int[] a){
+
+    }
 
 }
